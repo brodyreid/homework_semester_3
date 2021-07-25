955b5199b8dbfcb50c20ef197152eb949541dc3e
brodyreid <brodyhreid@gmail.com>
2021-07-25 15:51:01 -0400

    added collision detection and piece rotation

--- homework_3/tetris/src/hooks/usePlayer.js
+++ homework_3/tetris/src/hooks/usePlayer.js
@@ -1,13 +1,41 @@
 import { useState, useCallback } from 'react';
-import { STAGE_WIDTH } from '../gameHelpers';
-import { randomTetromino } from '../tetrominos';
+import { checkCollision, STAGE_WIDTH } from '../gameHelpers';
+import { TETROMINOS, randomTetromino } from '../tetrominos';
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0},
-        tetromino: randomTetromino().shape,
+        tetromino: TETROMINOS[0].shape,
         collided: false
     });
+
+    const rotate = (matrix, dir) => {
+        const rotatedTetro = matrix.map((_, index) =>
+            matrix.map(col => col[index])
+        );
+        if (dir > 0) return rotatedTetro.map(row => row.reverse())
+        return rotatedTetro.reverse()
+    }
+
+    const playerRotate = (stage, dir) => {
+        const copyPlayer = JSON.parse(JSON.stringify(player))
+        copyPlayer.tetromino = rotate(copyPlayer.tetromino, dir)
+
+        // Fix rotations near boundary
+        const pos = copyPlayer.pos.x
+        let offset = 1
+        while(checkCollision(copyPlayer, stage, { x: 0, y: 0})) {
+            copyPlayer.pos.x += offset
+            offset = -(offset + (offset > 0 ? 1 : -1))
+            if (offset > copyPlayer.tetromino[0].length) {
+                rotate(copyPlayer.tetromino, -dir)
+                copyPlayer.pos.x = pos
+                return
+            }
+        }
+
+        setPlayer(copyPlayer)
+    }
 
     const updatePlayerPos = ({ x, y, collided}) => {
         setPlayer(prev => ({
@@ -25,5 +53,5 @@ export const usePlayer = () => {
         })
     }, [])
 
-    return [player, updatePlayerPos, resetPlayer];
+    return [player, updatePlayerPos, resetPlayer, playerRotate];
 }
